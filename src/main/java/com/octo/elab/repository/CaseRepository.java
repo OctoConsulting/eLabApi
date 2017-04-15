9caffd29ea592c414950597ef20f2b5d5f89325d
sumitdang <sumit.dang@octoconsulting.com>
2017-04-15 13:15:02 -0400

    Case Repo

--- src/main/java/com/octo/elab/repository/CaseRepository.java
+++ src/main/java/com/octo/elab/repository/CaseRepository.java
@@ -10,16 +10,10 @@ import com.octo.elab.pojo.db.Case;
 
 public interface CaseRepository extends JpaRepository<Case, Integer> {
 
-	@Query(value = "select c.* from elab.Case c where (c._id = :CaseID)", nativeQuery = true)
+	@Query(value = "select c.* from elab.Case c where (c.id = :CaseID)", nativeQuery = true)
 	public Case getCaseByID(@Param("CaseID") Integer CaseID);
 
-	@Query(value = "select c.* from elab.Case c order by c._id", nativeQuery = true)
+	@Query(value = "select c.* from elab.Case c order by c.id", nativeQuery = true)
 	public List<Case> getAllCases();
-
-	@Query(value = "select c._id from elab.Case c order by c._id", nativeQuery = true)
-	public Integer[] getAllCaseIDs();
-
-	@Query(value = "select max(_id) from elab.Case", nativeQuery = true)
-	public Integer getMaxCaseID();
 
 }
