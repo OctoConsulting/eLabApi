259a13eda491615455b750718b6d999472cd12de
akanksha chauhan <akanksha.chauhan@octoconsulting.com>
2017-04-18 18:49:05 -0400

    Evidence fixed

--- src/main/java/com/octo/elab/pojo/db/Evidence.java
+++ src/main/java/com/octo/elab/pojo/db/Evidence.java
@@ -9,6 +9,7 @@ import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.octo.elab.utilities.CustomDateTimeDeserializer;
