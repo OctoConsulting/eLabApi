ead0207c9f42ad49d65e31b25186e22455fe8eb7
sumitdang <sumit.dang@octoconsulting.com>
2017-04-27 11:48:06 -0400

    Parameter name

--- src/main/java/com/octo/elab/pojo/reflection/EvidenceNew.java
+++ src/main/java/com/octo/elab/pojo/reflection/EvidenceNew.java
@@ -10,38 +10,47 @@ public class EvidenceNew {
 	private List<AccessPair> evidenceType = null;
 	private List<AccessPair> parentType = null;
 	private List<AccessPair> parentEvidenceNumber = null;
-	private String Name = null;
-	private Boolean forAnalysis =  null;
-	
-	public List<AccessPair> getParentEvidenceNumber(){
+	private String name = null;
+	private Boolean forAnalysis = null;
+
+	public List<AccessPair> getParentEvidenceNumber() {
 		return parentEvidenceNumber;
 	}
+
 	public void setParentEvidenceNumber(List<AccessPair> parentEvidenceNumber) {
 		this.parentEvidenceNumber = parentEvidenceNumber;
 	}
+
 	public List<AccessPair> getParentType() {
 		return parentType;
 	}
+
 	public void setParentType(List<AccessPair> parentType) {
 		this.parentType = parentType;
 	}
+
 	public List<AccessPair> getEvidenceType() {
 		return evidenceType;
 	}
+
 	public void setEvidenceType(List<AccessPair> evidenceType) {
 		this.evidenceType = evidenceType;
 	}
+
 	public Boolean getForAnalysis() {
 		return forAnalysis;
 	}
+
 	public void setForAnalysis(Boolean forAnalysis) {
 		this.forAnalysis = forAnalysis;
 	}
+
 	public String getName() {
-		return Name;
+		return name;
 	}
+
 	public void setName(String name) {
-		Name = name;
+		this.name = name;
 	}
-	
+
 }
