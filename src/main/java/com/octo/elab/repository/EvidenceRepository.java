4c9021bc437ab5839b88217e1e689ad58f596d66
akanksha chauhan <akanksha.chauhan@octoconsulting.com>
2017-05-09 16:10:36 -0400

    Order by added
    Order by added

--- src/main/java/com/octo/elab/repository/EvidenceRepository.java
+++ src/main/java/com/octo/elab/repository/EvidenceRepository.java
@@ -10,40 +10,40 @@ import com.octo.elab.pojo.db.Evidence;
 
 public interface EvidenceRepository extends JpaRepository<Evidence, Integer> {
 
-	@Query(value = "select e.*, et.description from elab.evidence e inner join elab.evidence_type et on et.id = e.evidence_type where (e.id = :evidenceID)", nativeQuery = true)
+	@Query(value = "select e.*, et.description from elab.evidence e inner join elab.evidence_type et on et.id = e.evidence_type where (e.id = :evidenceID) order by e.id", nativeQuery = true)
 	public Evidence getEvidenceByID(@Param("evidenceID") Integer evidenceID);
 
 	@Query(value = "select e.*, et.description from elab.evidence e inner join elab.evidence_type et on et.id = e.evidence_type order by e.id", nativeQuery = true)
 	public List<Evidence> getAllEvidences();
 	
-	@Query(value = "select e.*, et.description from elab.evidence e inner join elab.evidence_type et on et.id = e.evidence_type where (e.parent_id = :parentID)", nativeQuery = true)
+	@Query(value = "select e.*, et.description from elab.evidence e inner join elab.evidence_type et on et.id = e.evidence_type where (e.parent_id = :parentID) order by e.id", nativeQuery = true)
 	public List<Evidence> getEvidencesByParentID(@Param("parentID") Integer parentID);
 	
-	@Query(value = "select e.*, et.description from elab.evidence e inner join elab.evidence_type et on et.id = e.evidence_type where (e.case_id = :caseID)", nativeQuery = true)
+	@Query(value = "select e.*, et.description from elab.evidence e inner join elab.evidence_type et on et.id = e.evidence_type where (e.case_id = :caseID) order by e.id", nativeQuery = true)
 	public List<Evidence> getEvidenceByCaseID(@Param("caseID") Integer caseID);
 	
-	@Query(value = "select e.*, et.description from elab.evidence e inner join elab.evidence_type et on et.id = e.evidence_type where (e._id in :evidenceIDs)", nativeQuery = true)
+	@Query(value = "select e.*, et.description from elab.evidence e inner join elab.evidence_type et on et.id = e.evidence_type where (e._id in :evidenceIDs) order by e.id", nativeQuery = true)
 	public List<Evidence> getEvidencesByID(@Param("evidenceIDs") Integer[] evidenceIDs);
 	
-	@Query(value = "select e.*, et.description from elab.evidence e inner join elab.evidence_type et on et.id = e.evidence_type where (e.case_id = :caseID) and e.evidence_type = 1", nativeQuery = true)
+	@Query(value = "select e.*, et.description from elab.evidence e inner join elab.evidence_type et on et.id = e.evidence_type where (e.case_id = :caseID) and e.evidence_type = 1 order by e.id", nativeQuery = true)
 	public List<Evidence> getContainersByCaseID(@Param("caseID") Integer caseID);
 	
-	@Query(value = "select e.*, et.description from elab.evidence e inner join elab.evidence_type et on et.id = e.evidence_type where e.evidence_type = 3 and e.is_foranalysis = true", nativeQuery = true)
+	@Query(value = "select e.*, et.description from elab.evidence e inner join elab.evidence_type et on et.id = e.evidence_type where e.evidence_type = 3 and e.is_foranalysis = true order by e.id", nativeQuery = true)
 	public List<Evidence> getAllEvidencesForExam();
 	
-	@Query(value = "select max(id) from elab.evidence", nativeQuery = true)
+	@Query(value = "select max(id) from elab.evidence order by e.id", nativeQuery = true)
 	public Integer getMaxEvidenceID();
 	
-	@Query(value = "select * from elab.evidence e where e.case_id = :caseID and e.evidence_type = :evidenceType", nativeQuery = true)
+	@Query(value = "select * from elab.evidence e where e.case_id = :caseID and e.evidence_type = :evidenceType order by e.id", nativeQuery = true)
 	public List<Evidence> getEvidencesByEvidenceTypeForCaseID(@Param("caseID") Integer caseID, @Param("evidenceType") Integer evidenceType);
 	
-	@Query(value = "select max(_id) from elab.evidence where evidence_type = :evidenceType", nativeQuery = true)
+	@Query(value = "select max(_id) from elab.evidence where evidence_type = :evidenceType order by e.id", nativeQuery = true)
 	public Integer getMaxEvidence_ID(@Param("evidenceType") Integer evidenceType);
 	
-	@Query(value = "select e.*, et.description from elab.evidence e inner join elab.evidence_type et on et.id = e.evidence_type where (e.case_id = :caseID) and e.is_foranalysis = true", nativeQuery = true)
+	@Query(value = "select e.*, et.description from elab.evidence e inner join elab.evidence_type et on et.id = e.evidence_type where (e.case_id = :caseID) and e.is_foranalysis = true order by e.id", nativeQuery = true)
 	public List<Evidence> getEvidenceByCaseIDAndForAnalysis(@Param("caseID") Integer caseID);
 	
-	@Query(value = "select e.* from elab.evidence e where e.id in :ids",nativeQuery = true)
+	@Query(value = "select e.* from elab.evidence e where e.id in :ids order by e.id",nativeQuery = true)
 	public List<Evidence> getEvidenceByIds(@Param("ids")List<Integer> ids);
 
 }
